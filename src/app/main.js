713acc7800c696f4cb49f5df89bd809b232153a4
rayvector <rayvectorspqr@yahoo.com>
2020-05-06 12:19:24 +0300

    new el

--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,10 @@
 import SoloJS from "../CORE/SoloJS";
 import el_helloWorld from "./els/el_helloWorld";
+import el_userName from "./els/el_userName";
+import el_userNameResult from "./els/el_userNameResult";
 
 const SJS = new SoloJS("app");
 
-SJS.addEl(el_helloWorld);
+SJS.el(el_helloWorld);
+SJS.el(el_userName);
+SJS.el(el_userNameResult);
