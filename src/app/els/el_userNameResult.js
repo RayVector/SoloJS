cb7efe2834c6e11375f796b552eefc9115f702d0
rayvector <rayvectorspqr@yahoo.com>
2020-05-07 17:06:23 +0300

    el refactor

--- src/app/els/el_userNameResult.js
+++ src/app/els/el_userNameResult.js
@@ -1,15 +1,18 @@
+const styles = {
+  marginLeft: '10px',
+  fontSize: '15px',
+  color: 'gray'
+};
+
+
 export default {
   content: 'User Name:',
   childList: [
     {
       id: 'userNameResult',
       node: 'span',
-      styles: {
-        marginLeft: '10px',
-        fontSize: '15px',
-        color: 'gray'
-      },
-      content: 'Type user name in the field'
+      styles,
+      content: 'Type user name in the field',
     }
   ]
 }
